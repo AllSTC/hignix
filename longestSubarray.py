b416ea8792b8e96d7d8437687da5def512f85368
AllSTC <145006528+AllSTC@users.noreply.github.com>
2026-09-02 16:50:38 +0530

    Implement subarray product and minimum length functions
    Added functions to calculate subarray products and minimum subarray length.

--- longestSubarray.py
+++ longestSubarray.py
@@ -10,3 +10,31 @@ class Solution:
             left+=1
           max_len = max(max_len,right-left+1)
     return max_len
+    #subproduct less than k
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k<=1:
+            return 0
+        l,rp,res = 0,1,0
+        for r in range(len(nums)):
+            rp *= nums[r]
+            while rp>=k:
+                rp /= nums[l]
+                l+=1
+            res+=r-l+1
+        return res
+      #whose sum is greater than or equal to target
+      class Solution:
+    def minSubArrayLen(self, k: int, nums: List[int]) -> int:
+        if sum(nums)<k:
+            return 0
+        l,csum ,res= 0,0,float('inf')
+        for r in range(len(nums)):
+            csum+=nums[r]
+            while csum>=k:
+                res=min(res,r-l+1)
+                csum-=nums[l]
+                l+=1
+
+        return res
+        
